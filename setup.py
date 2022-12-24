428ac175fad6c33c058a392c7eb4bd34c6874cc1
NoorCrony <mahammadnoor122333@gmail.com>
2022-12-24 11:59:31 +0530

    updated 1

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import find_packages, setup
+from typing import List
+
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = 
+
 
 setup(
     name="sensor",
@@ -7,5 +16,5 @@ setup(
     author_email="mahammadnoor122333@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements(),
-    
+
 )
